768775bf4e4bfd96e27341b2f1af6c6d129d2ca2
Dmitriy <argizol@mail.ru>
2022-05-06 20:24:05 +0300

    Lesson 2

---
+++ Lesson2/MonthTemp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+
+    //Считаем и получаем среднюю температуру
+    internal class MothTemp 
+    {
+
+        public static int MiddleTemp()
+        {
+            Console.WriteLine("Введите минимальную температуры за день: ");
+            int minTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуры за день: ");
+            int maxTemp = Convert.ToInt32(Console.ReadLine());
+            return (minTemp + maxTemp) / 2;
+        }
+
+        //Проверяем что месяц зимний и средняя температура больше 0, затем печатаем результат
+        public static void PrintWintMonth(){
+            Console.WriteLine("Введите порядковый номер текущего месяца: ");
+            int MonthNum = Convert.ToInt32(Console.ReadLine());
+            if ((MonthNum == 11 || MonthNum == 12 || MonthNum == 1) && MiddleTemp() > 0)
+            {
+                Console.WriteLine("Дождливая зима");
+            } else
+            {
+                Console.WriteLine("Не зима и, вероятно, не дождливая");
+            }
+        } 
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class Homework2
+    {
+         static void Main(string[] args)
+        {
+
+            // MiddleTemp();
+            //TodayMonth();
+            //IsNumEven();
+            //MothTemp.PrintWintMonth();
+            Chek.PrintChek();
+            Console.ReadLine();
+            
+        }
+
+        //Считаем среднюю температуру
+        public static void MiddleTemp()
+        {
+            Console.WriteLine("Введите минимальную температуры за день: ");
+            int minTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуры за день: ");
+            int maxTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Среднесуточная температура: {(minTemp + maxTemp) / 2}");
+        }
+
+        //Определяем месяц по введенному номеру
+        public static void TodayMonth()
+        {
+            Console.WriteLine("Введите порядковый номер текущего месяца: ");
+             int todayMonth = Convert.ToInt32(Console.ReadLine());
+             switch (todayMonth)
+             {
+                 case 1: Console.WriteLine("Январь");
+                     break;
+                 case 2: Console.WriteLine("Февраль");
+                     break;
+                 case 3: Console.WriteLine("Март");
+                     break;
+                 case 4:
+                     Console.WriteLine("Апрель");
+                     break;
+                 case 5:
+                     Console.WriteLine("Май");
+                     break;
+                 case 6:
+                     Console.WriteLine("Июнь");
+                     break;
+                 case 7:
+                     Console.WriteLine("Июль");
+                     break;
+                 case 8:
+                     Console.WriteLine("Август");
+                     break;
+                 case 9:
+                     Console.WriteLine("Сентябрь");
+                     break;
+                 case 10:
+                     Console.WriteLine("Октябрь");
+                     break;
+                 case 11:
+                     Console.WriteLine("Ноябрь");
+                     break;
+                 case 12:
+                     Console.WriteLine("Декабрь");
+                     break;
+                 default: Console.WriteLine("Введите число от 1 до 12");
+                     break;
+             }
+        }
+
+        //Определяем четность числа
+        public static void IsNumEven()
+        {
+            Console.WriteLine("Введите число: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(" Число четное ");
+                return;
+            }
+            Console.WriteLine("Число нечетное");
+        }
+    }
+}
