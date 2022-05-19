aa266bbb8716cfcf0bad824f99ea64f760b298b7
Dmitriy <argizol@mail.ru>
2022-05-19 21:01:34 +0300

    123

---
+++ Lesson4/Task2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    internal class Task2
+    {
+        
+        public static string getString()
+        {
+            Console.WriteLine("Введите цифры разделенные пробелом ");
+            return Console.ReadLine(); ;
+        }
+        public static void stringToInt(string stroka)
+        {            
+            string[] words = stroka.Split();
+            int sum = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                sum += int.Parse(words[i]);
+            }
+            Console.WriteLine($"Сумма :{sum}");
+        }
+
+
+
+    }
+
+
+    }
+
