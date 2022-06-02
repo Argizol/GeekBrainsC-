d80197f75644e02ab6451840d0f93472f21bb7f2
Dmitriy <argizol@mail.ru>
2022-06-02 16:08:34 +0300

    ДЗ 8

---
+++ Lesson8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+                Console.WriteLine("Введите имя пользователя:");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Console.WriteLine("Введите возраст пользователя:");
+                Properties.Settings.Default.UserAge = Console.ReadLine();
+                Console.WriteLine("Введите профессию пользователя:");
+                Properties.Settings.Default.UserProffecy = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            string HelloByStart = Properties.Settings.Default.HelloByStart;
+            string UserName = Properties.Settings.Default.UserName;            
+            string UserAge = Properties.Settings.Default.UserAge;
+            string UserProffecy = Properties.Settings.Default.UserProffecy;
+            Console.WriteLine($"{HelloByStart}, {UserName}! \n Возраст: {UserAge} \n Род деятельности: {UserProffecy}");
+            Console.ReadKey(true);
+        }
+        
+    }
+}
+
